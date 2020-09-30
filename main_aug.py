535359ce97fcabd8bd21a4aa5f428565dd0af8e0
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-09-30 17:55:00 +0200

    q4 done

--- main_aug.py
+++ main_aug.py
@@ -8,7 +8,7 @@ from simulation import EmbeddedSimEnvironment
 # Create quadrotor and controller objects
 quad = Quadrotor()
 
-# ASTRID IF YOU CHANGE THE REFERNCE HERE YOU ALSO HAVE TO CHANGE THE VALUES ON LINES 96, 97 and 98
+# ASTRID IF YOU CHANGE THE REFERNCE DO IT HERE
 #                             x    y    z
 quad.set_reference(np.array([0.5, 0.3, 0.5]))
 
@@ -36,7 +36,7 @@ ctl = MPC(model=quad,
           dynamics=quad.discrete_time_aug_dynamics,
           Q=Q, R=R, P=P,
           horizon=5,
-          disturb_thrust=0.5,  # ASTRID PLAY WITH THIS
+          disturb_thrust=0.5,  # ASTRID PLAY WITH THIS (THE THRUST DISTURBANCE)
           ulb=[-f_max, -tau_max, -tau_max, -tau_z_max],
           uub=[f_max, tau_max, tau_max, tau_z_max],
           xlb=[
@@ -85,8 +85,7 @@ def dummy_ctrl(x, u0=None):
     return np.array([[0.0, -0.000, 0.01, 0.000]]).T
 
 
-# quad = Quadrotor(m=5)
-quad.m = 2  # ASTRID PLAY WITH THIS
+quad.m = 2  # ASTRID PLAY WITH THIS (THE MASS)
 sim_env = EmbeddedSimEnvironment(model=quad,
                                  dynamics=quad.discrete_time_nl_dynamics,
                                  controller=ctl.mpc_controller,
@@ -110,41 +109,3 @@ ctl.set_reference([
     0   # rz
 ])
 t, y, u = sim_env.run([0] * 15)
-
-# Part III - Full cart model
-# pendulum = Pendulum()
-
-# # Get the system discrete-time dynamics
-# A, B, Bw, C = pendulum.get_discrete_system_matrices_at_eq()
-
-# # Solve the ARE for our system to extract the terminal weight matrix P
-# Q = np.eye(4)*1
-# R = np.eye(1)*1
-# P = np.eye(4)*1
-
-# # Instantiate controller
-# ctl = MPC(model=pendulum,
-#           dynamics=pendulum.discrete_time_dynamics,
-#           horizon=7,
-#           Q = Q , R = R, P = P,
-#           ulb=-5, uub=5,
-#           xlb=[-2, -10, -np.pi/2, -np.pi/2],
-#           xub=[12, 10, np.pi/2, np.pi/2])
-
-# # Solve without disturbance
-# ctl.set_reference(x_sp=np.array([10,0,0,0]))
-# sim_env_full = EmbeddedSimEnvironment(model=pendulum,
-#                                 dynamics=pendulum.pendulum_linear_dynamics_with_disturbance,
-#                                 controller=ctl.mpc_controller,
-#                                 time = 6)
-# sim_env_full.run([0,0,0,0])
-
-# # Solve witho disturbance
-# pendulum.enable_disturbance(w=0.05)
-# ctl.set_reference(x_sp=np.array([10,0,0,0]))
-# sim_env_full_dist = EmbeddedSimEnvironment(model=pendulum,
-#                                 dynamics=pendulum.discrete_time_dynamics,
-#                                 controller=ctl.mpc_controller,
-#                                 time = 10)
-# sim_env_full_dist.set_window(5)
-# sim_env_full_dist.run([0,0,0,0])
